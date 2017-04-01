e21d2ce35c3c5d77ca83399bd502867a071fd104
administrator <liquidnuker@users.noreply.github.com>
2017-03-31 20:48:13 -0800

    added other modules

--- mediator--trigger.js
+++ mediator--trigger.js
@@ -2,16 +2,31 @@ var mediator = new Mediator();
 
 // module1
 mediator.subscribe("uppercaser", function (arg) {
-  console.log(arg.toUpperCase());
+  mediator.publish("splitter", arg.toUpperCase());
+  mediator.publish("log", "module1 done");
 });
 
 // module2
-mediator.subscribe("message", function (arg) {
-  console.log(arg);
+mediator.subscribe("splitter", function (arg) {
+  var array1 = arg.split("");
+  mediator.publish("length", array1);
+  mediator.publish("log", "module2 done " + new Date());
 });
 
-mediator.publish("message", "message1");
-mediator.publish("uppercaser", "ytruytuy");
+// module3 
+mediator.subscribe("length", function (arg) {
+  var l = arg.length;
+  mediator.publish("log", l + " module3 done " + new Date());
+});
 
-// mediator.remove("message"); 
-mediator.publish("message", "message1");
+// log
+mediator.subscribe("log", function (arg) {
+  var log = [];
+  log.push(arg);
+  document.write(log.toString().split(""));
+});
+
+
+// start
+var var1 = "xcxxcxc";
+mediator.publish("uppercaser", var1);
